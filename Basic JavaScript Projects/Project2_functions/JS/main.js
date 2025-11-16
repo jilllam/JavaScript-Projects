856dbdf33c73c22608870e22e230bae0789a00bf
Jilam <lyljilllam@gmail.com>
2025-11-16 16:49:39 +0000

    Add Project2_functions folder with JS and HTML files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//a function taht uses the += operator
+function MyFunction() {
+    var str1 = "Hello!"; //the variable
+    str1 += " This is my first function!"; //using += to add to the variable
+    document.getElementById("Button_Text").innerHTML = str1; // using document.getElementById()
+}
